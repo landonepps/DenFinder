f9f7c95b37cf9332107e0061aaefe5ee0460be2e
Landon Epps <landon.epps@gmail.com>
2014-10-05 23:53:38 -0500

    Form now sends address to results.html

--- src/main/java/denfinder/controllers/MainController.java
+++ src/main/java/denfinder/controllers/MainController.java
@@ -1,14 +1,17 @@
 package denfinder.controllers;
 
 
+import denfinder.model.FormData;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
 	@RequestMapping("/")
-	public String home() {
-
+	public String home(Model model) {
+        model.addAttribute("formData", new FormData());
 		return "index";
 	}
 	
@@ -17,4 +20,10 @@ public class MainController {
 
 		return "map";
 	}
+
+    @RequestMapping(value="/results", method=RequestMethod.GET)
+    public String results() {
+
+        return "results";
+    }
 }
---
+++ src/main/java/denfinder/model/FormData.java
@@ -0,0 +1,16 @@
+package denfinder.model;
+
+/**
+ * Created by landon on 10/5/14.
+ */
+public class FormData {
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        address = address;
+    }
+
+    private String address;
+}
