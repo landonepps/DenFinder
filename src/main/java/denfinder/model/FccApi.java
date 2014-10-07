185698c22ac020bdc33f008482391bb1504a959a
Landon Epps <landon.epps@gmail.com>
2014-10-06 20:53:16 -0500

    Added FccApi.java

---
+++ src/main/java/denfinder/model/FccApi.java
@@ -0,0 +1,21 @@
+package denfinder.model;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+/**
+ * Created by landon on 10/6/14.
+ */
+public class FccApi {
+    public static String getFIPSCode(double lat, double lon) throws IOException {
+        String url = "http://data.fcc.gov/api/block/find?latitude=" +
+                lat + "&longitude=" + lon +" &showall=false&format=json";
+
+        JSONObject json = ApiCall.loadJSON(url);
+
+        System.out.println(json);
+        //TODO 
+        return new String();
+    }
+}
